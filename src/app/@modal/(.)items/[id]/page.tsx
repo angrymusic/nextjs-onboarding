ce2d30666ee98eae9996894b7de188ddb172e5f5
angrymusic <angrymusic@naver.com>
2026-09-21 14:22:16 +0900

    feat: dialog용 summary 구현

--- src/app/@modal/(.)items/[id]/page.tsx
+++ src/app/@modal/(.)items/[id]/page.tsx
@@ -1,5 +1,5 @@
 import Modal from "@/app/components/common/modal";
-import Info from "@/app/components/items/info";
+import InfoSummary from "@/app/components/items/info-summary";
 import { db } from "@/lib/db";
 import { notFound } from "next/navigation";
 
@@ -19,7 +19,7 @@ export default async function ItemDetailModal({ params }: Props) {
 
   return (
     <Modal>
-      <Info item={item} />
+      <InfoSummary item={item} />
       <a href={`/items/${id}`} rel="noopener noreferrer">
         [풀페이지로 보기]
       </a>
